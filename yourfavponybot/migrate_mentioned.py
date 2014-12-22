d269dfd228b5ff624394e1a689d99a4d5b3d2078
Gian Sass <gian.sass@outlook.de>
2014-12-22 19:15:52 +0100

    Refactor migrate_mentioned.py

--- yourfavponybot/migrate_mentioned.py
+++ yourfavponybot/migrate_mentioned.py
@@ -1,10 +1,28 @@
+"""This script migrates a mentioned.txt file to a sqlite3 database"""
+
 from argparse import ArgumentParser
 import sqlite3
 
 def main():
+    """
+    The main method
+    """
     parser = ArgumentParser()
-    parser.add_argument("-i", "--input", required=True, help="The mentioned.txt file you want to migrate to SQL", metavar="FILE")
-    parser.add_argument("-o", "--output", required=False, help="The output filename of the DB", metavar="FILE", default="mentioned.db")
+    parser.add_argument(
+        "-i",
+        "--input",
+        required=True,
+        help="The mentioned.txt file you want to migrate to SQL",
+        metavar="FILE"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        required=False,
+        help="The output filename of the DB",
+        metavar="FILE",
+        default="mentioned.db"
+    )
     args = parser.parse_args()
 
     conn = sqlite3.connect(args.output)
@@ -12,15 +30,15 @@ def main():
 
     cursor.execute("CREATE TABLE IF NOT EXISTS mentioned (id INTEGER)")
 
-    with open(args.input, "r") as file:
+    with open(args.input, "r") as input_file:
         values = []
-        for line in file.read().splitlines():
+        for line in input_file.read().splitlines():
             try:
-                id = int(line)
+                num = int(line)
             except ValueError:
                 pass
             finally:
-                values.append((id,))
+                values.append((num,))
 
         cursor.executemany("INSERT INTO mentioned VALUES (?)", values)
         conn.commit()
